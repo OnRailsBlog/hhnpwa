49c5b105b57e90c7c16b326ca690d148284b17a8
john@onrails.blog <john@onrails.blog>
2020-12-23 11:17:48 -0500

    Adding Turbo
    Signed-off-by: john@onrails.blog <john@onrails.blog>

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,12 +4,11 @@
 // that code so it'll be compiled.
 
 import Rails from "@rails/ujs";
-import Turbolinks from "turbolinks";
 import * as ActiveStorage from "@rails/activestorage";
 import "channels";
+import { Turbo, cable } from "@hotwired/turbo-rails";
 
 Rails.start();
-Turbolinks.start();
 ActiveStorage.start();
 
 require("../stylesheets/application.scss");
