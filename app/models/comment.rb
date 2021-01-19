6e228f6a03b6ab0a49f4dd8335c5d3cc0ca94be8
john@onrails.blog <john@onrails.blog>
2021-01-19 13:57:20 -0500

    Improving Performance with Russian Doll Caching HOTWire Tutorial 6
    Signed-off-by: john@onrails.blog <john@onrails.blog>

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,7 @@
 class Comment < ApplicationRecord
   has_one :hn_parent, class_name: 'Comment', primary_key: 'parent_id', foreign_key: 'hn_id'
   has_many :kids, class_name: "Comment", primary_key: 'hn_id', foreign_key: 'parent_id'
+  after_save :update_parent
   
   def populate(json) 
     if json.nil?
@@ -12,5 +13,9 @@ class Comment < ApplicationRecord
     self.text = json['text'] if json['text']
     self.dead = json['dead'] if json['dead']
     self.time = DateTime.strptime("#{json['time']}",'%s') if json['time']
+  end
+  
+  def update_parent
+    hn_parent.touch if hn_parent.present?
   end
 end
